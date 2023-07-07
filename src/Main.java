0aace2b9f736e8f168d48ee9ecac97fdeb87e0bd
engineeranil <120477702+engineeranil@users.noreply.github.com>
2023-07-07 23:05:17 +0300

    TypeConversion

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        String abc = String.valueOf(a);
+        System.out.println(abc);
+        double b = (int)a;
+        System.out.println(b);
+        char ab  = 'd';
+        int d = (char)ab;
+        System.out.println(d);
+
+    }
+}
